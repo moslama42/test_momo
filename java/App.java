4a0991b6f42b9810282da3bf745a256b8c36e565
mohamed pcBureauHP <m.slama.pro@gmail.com>
2022-11-28 17:08:14 +0100

    mon premier commit de projet java

---
+++ java/App.java
@@ -0,0 +1,46 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Planete mercure = new Planete();
+        mercure.nom = "Mercure";
+        mercure.diametre = 4880;
+        mercure.matiere = "Tellurique";
+
+        Planete venus = new Planete();
+        venus.nom = "Venus";
+        venus.diametre = 12100;
+        venus.matiere = "volcanique";
+
+        Planete terre = new Planete();
+        terre.nom = "Terre";
+        terre.diametre = 12756;
+        terre.matiere = "Tellurique";
+
+        Planete mars = new Planete();
+        mars.nom = "Mars";
+        mars.diametre = 6792;
+        mars.matiere = "Tellurique";
+
+        Planete jupiter = new Planete();
+        jupiter.nom = "Jupiter";
+        jupiter.diametre = 142984;
+        jupiter.matiere = "Gazeuse";
+
+        Planete saturne = new Planete();
+        saturne.nom = "Saturne";
+        saturne.diametre = 120536;
+        saturne.matiere = "Gazeuse";
+
+        Planete uranus = new Planete();
+        uranus.nom = "Uranus";
+        uranus.diametre = 51118;
+        uranus.matiere = "Gazeuse";
+
+        Planete neptune = new Planete();
+        neptune.nom = "Neptune";
+        neptune.diametre = 49532;
+        neptune.matiere = "Gazeuse";
+
+        jupiter.revolution();
+        mars.rotation();
+    }
+}
---
+++ java/Plantete.java
@@ -0,0 +1,14 @@
+public class Planete {
+    String nom;
+    String matiere;
+    int diametre;
+
+    void revolution() {
+        System.out.println("Je suis la palnète " + nom + " et je tourne autour de mon étoile.");
+    }
+
+    void rotation() {
+        System.out.println("Je suis la planète " + nom + "  et je tourne sur moi-même.");
+    }
+
+}
